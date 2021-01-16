be6184a57b48d4fb732c8882a1b9f43a391e74e0
philbowden <phil.g.bowden@gmail.com>
2021-01-16 00:40:29 -0800

    EditForm added.

--- philsblog/blogoramma/views.py
+++ philsblog/blogoramma/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from .forms import PostForm, EditForm
 from .models import Post
-from .forms import PostForm
 
 
-#def home(request):
+# def home(request):
 #	return render(request, 'home.html', {})
 
 class HomeView(ListView):
-	model = Post
-	template_name = 'home.html'
+    model = Post
+    template_name = 'home.html'
 
 
 class ArticleDetailView(DetailView):
-	model = Post
-	template_name = 'article_details.html'	
+    model = Post
+    template_name = 'article_details.html'
+
 
 class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+
+
+# fields = '__all__'
+# fields = ('title', 'body')
+
+
+class UpdatePostView(UpdateView):
 	model = Post
-	form_class = PostForm
-	template_name = 'add_post.html'
-	#fields = '__all__'
-	#fields = ('title', 'body')
+	form_class = EditForm
+	template_name = 'update_post.html'
+    #fields = ['title', 'title_tag', 'body']
